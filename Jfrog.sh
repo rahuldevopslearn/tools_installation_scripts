720cdad1db72e8edcd4c3ca2b4417494066e62da
rahuldevopslearn <131506642+rahuldevopslearn@users.noreply.github.com>
2025-12-14 13:26:36 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -4,3 +4,15 @@ docker pull docker.bintray.io/jfrog/artifactory-oss:latest
 sudo mkdir -p /jfrog/artifactory
 sudo chown -R 1030 /jfrog/
 docker run --name artifactory -d -p 8081:8081 -p 8082:8082 -v /jfrog/artifactory:/var/opt/jfrog/artifactory docker.bintray.io/jfrog/artifactory-oss:latest
+
+docker volume create artifactory-data
+docker pull releases-docker.jfrog.io/jfrog/artifactory-oss:7.59.9
+docker run -d --name artifactory -p 8081:8081 -p 8082:8082 -v artifactory-data:/var/opt/jfrog/artifactory --restart unless-stopped releases-docker.jfrog.io/jfrog/artifactory-oss:7.59.9
+#Verify container & Java version
+docker ps
+docker logs artifactory | head
+docker exec -it artifactory java -version
+
+#Default credentials
+Username: admin
+Password: password
